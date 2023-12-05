1f1efe2e118818ca44e73120c660a3f2e39697a1
YoranLambert <yoran.lambert@student.howest.be>
2023-12-05 09:05:31 +0100

    Add project files.

---
+++ WX1try/Program.cs
@@ -0,0 +1,51 @@
+﻿using HiveMQtt.Client;
+using HiveMQtt.Client.Events;
+using HiveMQtt.Client.Options;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.Text.Json.Nodes;
+using System.Threading;
+
+namespace WX1try
+{
+    internal class Program
+    {
+        private static ManualResetEvent waitHandle = new ManualResetEvent(false);
+        public static HiveMQClient Client { get; set; }
+        static async Task Main(string[] args)
+        {
+            var options = new HiveMQClientOptions();
+            options.Host = "87161f7f14664f20a4a1d7d8fe033d62.s1.eu.hivemq.cloud";
+            options.Port = 8883;
+            options.UseTLS = true;
+            options.UserName = "wx1mqttdemo2324jh1";
+            options.Password = "4iJv8fiAdJ4mzcsbdw6i";
+
+            Client = new HiveMQClient(options);
+            await Client.ConnectAsync().ConfigureAwait(false);
+
+            await Client.SubscribeAsync("teams/list");
+            await Client.SubscribeAsync("teams/10/question/new");
+            await Client.SubscribeAsync("teams/10/question/error");
+            Client.OnMessageReceived += (sender, e) =>
+            {
+                OnMessageReceived(sender, e);
+            };
+            waitHandle.WaitOne();
+            await Client.DisconnectAsync().ConfigureAwait(false);
+        }
+        public static async void OnMessageReceived(object Sender, OnMessageReceivedEventArgs args)
+        {
+            var output = JsonConvert.DeserializeObject<Response>(args.PublishMessage.PayloadAsString);
+            Console.WriteLine(output.Question);
+            await Client.PublishAsync(output.TopicToPublishAnswerTo, Console.ReadLine());
+            waitHandle.Set();
+        }
+
+    }
+    public class Response
+    {
+        public string Question { get; set; }
+        public string TopicToPublishAnswerTo { get; set; }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using HiveMQtt.Client;
+using HiveMQtt.Client.Events;
+using HiveMQtt.Client.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public HiveMQClient Client;
+
+        public void OnMessageReceived(object Sender, OnMessageReceivedEventArgs args)
+        {
+            var output = JsonConvert.DeserializeObject<Response>(args.PublishMessage.PayloadAsString);
+            Console.WriteLine(output.Question);
+
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Connect();
+        }
+        private async void Connect()
+        {
+            var options = new HiveMQClientOptions();
+            options.Host = "ce6b74a7cccc45cb8db551fc2d7394bb.s2.eu.hivemq.cloud";
+            options.Port = 8883;
+            options.UseTLS = true;
+            options.UserName = "wx1mqttdemo2324jh1";
+            options.Password = "4iJv8fiAdJ4mzcsbdw6i";
+
+            Client = new HiveMQClient(options);
+            await Client.ConnectAsync().ConfigureAwait(false);
+
+            await Client.SubscribeAsync("teams/10/question/new");
+            await Client.SubscribeAsync("teams/10/question/error");
+            Client.OnMessageReceived += (sender, e) =>
+            {
+                OnMessageReceived(sender, e);
+            };
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Client.PublishAsync(output.TopicToPublishAnswerTo, Answer.Text);
+            await Client.DisconnectAsync().ConfigureAwait(false);
+        }
+    }
+    public class Response
+    {
+        public string Question { get; set; }
+        public string TopicToPublishAnswerTo { get; set; }
+    }
+}
